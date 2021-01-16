deacf2078c2dc87f435f45c9d96e901942cdae0a
Dubkov <yevgeniydubkov@gmail.com>
2021-01-16 18:48:53 +0300

    done

---
+++ lesson1/task2.py
@@ -0,0 +1,10 @@
+print('---')
+
+number = int(input('Введите время в секундах: '))
+hours = number // 3600
+h = number % 3600
+minutes = h // 60
+seconds =  h % 60
+print(f'Время - {hours} : {minutes} : {seconds}')
+
+print('---')

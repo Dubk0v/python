34704a3758508d2a99c4eaf9c219cb8f9d0ad189
Dubkov <yevgeniydubkov@gmail.com>
2021-01-16 22:33:51 +0300

    task 5 edit formula of profit task 6 edit

--- lesson1/task6.py
+++ lesson1/task6.py
@@ -1,11 +1,11 @@
 print('---')
 
-start = 2
-finish = 3
+start = int(input('Сколько пробежали в первый день: '))
+finish = int(input('Сколько км. вы хотите пробежать ? '))
 day = 1
 while start <= finish:
     start += ((start * 10) / 100)
     day += 1
-print(f'Спортсмен на {day} день достиг заданный результат - не менее', '%.0f' % start, 'км.')
+print(f'Спортсмен на {day} день достиг заданный результат - не менее {finish} км.')
 
 print('---')

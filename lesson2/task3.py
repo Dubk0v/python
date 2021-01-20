21a5b2e619af3adabda2e208327b966f40dcc075
Dubkov <yevgeniydubkov@gmail.com>
2021-01-20 19:07:11 +0300

    done task3

--- lesson2/task3.py
+++ lesson2/task3.py
@@ -6,17 +6,23 @@
 
 list_of_seasons = ['лето', 'зима', 'весна', 'осень']
 dict_of_seasons = {1: 'лето', 2: 'зима', 3: 'весна', 4: 'осень'}
-month = ('Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь')
+month = ('Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+         'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь')
 number = int(input('Введите месяц в виде целого числа от 1 до 12: '))
-while 0 <= number <= 12:
-
+if 0 < number <= 12:
     if 3 <= number <= 5:
-        print(f'{month[number - 1]} относится к {list_of_seasons[2]}')
+        print(f'list - {month[number - 1]} относится к времени года: {list_of_seasons[2]}')
+        print(f'dict - {month[number - 1]} относится к времени года: {dict_of_seasons.get(3)}')
     elif 6 <= number <= 8:
-        print(f'{month[number - 1]} относится к {list_of_seasons[0]}')
+        print(f'list - {month[number - 1]} относится к времени года: {list_of_seasons[0]}')
+        print(f'dict - {month[number - 1]} относится к времени года: {dict_of_seasons.get(1)}')
     elif 9 <= number <= 11:
-        print(f'{month[number - 1]} относится к {list_of_seasons[3]}')
+        print(f'list - {month[number - 1]} относится к времени года: {list_of_seasons[3]}')
+        print(f'dict - {month[number - 1]} относится к времени года: {dict_of_seasons.get(4)}')
     else:
-        print(f'{month[number - 1]} относится к {list_of_seasons[1]}')
+        print(f'list - {month[number - 1]} относится к времени года: {list_of_seasons[1]}')
+        print(f'dict - {month[number - 1]} относится к времени года: {dict_of_seasons.get(2)}')
+else:
+    print('Такого месяца не существует!!!')
 
-number = int(input('Введите месяц в виде целого числа от 1 до 12: '))
+

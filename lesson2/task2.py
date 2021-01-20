d4bc3d04ffdadbed761495b03660a1dd0d54bf18
Dubkov <yevgeniydubkov@gmail.com>
2021-01-21 02:17:46 +0300

    done task2

--- lesson2/task2.py
+++ lesson2/task2.py
@@ -4,3 +4,17 @@
 При нечетном количестве элементов последний сохранить на своем месте.
 Для заполнения списка элементов необходимо использовать функцию input().
 '''
+
+ask_list = list(input(':'))
+
+if len(ask_list) % 2 == 0:
+    print(ask_list)
+    ask_list[::2], ask_list[1::2] = ask_list[1::2], ask_list[::2]
+    print(ask_list)
+else:
+    print(ask_list)
+    al = ask_list.pop()
+    ask_list[::2], ask_list[1::2] = ask_list[1::2], ask_list[::2]
+    ask_list.append(al)
+    print(ask_list)
+

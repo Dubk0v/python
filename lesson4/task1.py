834733093e93242dd5f48a00f542dacccfafdffe
Dubkov <yevgeniydubkov@gmail.com>
2021-01-27 15:01:35 +0300

    done task

--- lesson4/task1.py
+++ lesson4/task1.py
@@ -5,4 +5,22 @@
 '''
 
 
+from sys import argv
 
+time = float(argv[1][argv[1].find('=')+1:])
+pay_rate = float(argv[2][argv[2].find('=')+1:])
+bonus = int(argv[3][argv[3].find('=')+1:])
+salary = time * pay_rate + bonus
+print(f'Расчёт заработной платы {salary} y.e.')
+
+# def salary():
+#     try:
+#         time = float(input('выработка в часах: '))
+#         pay_rate = float(input('ставка в час: '))
+#         bonus = int(input('премия: '))
+#         return time * pay_rate + bonus
+#     except ValueError:
+#         print('Вводи числа!!!')
+#
+#
+# print(f'Расчёт заработной платы {salary()} у.е.')

6418862a4c00adb679e44eca6a3fcc7e4c91b20a
Dubkov <yevgeniydubkov@gmail.com>
2021-01-28 23:48:46 +0300

    edit

--- lesson4/task1.py
+++ lesson4/task1.py
@@ -4,23 +4,19 @@
 Во время выполнения расчёта для конкретных значений необходимо запускать скрипт с параметрами.
 '''
 
-
 from sys import argv
 
-time = float(argv[1][argv[1].find('=')+1:])
-pay_rate = float(argv[2][argv[2].find('=')+1:])
-bonus = int(argv[3][argv[3].find('=')+1:])
-salary = time * pay_rate + bonus
-print(f'Расчёт заработной платы {salary} y.e.')
+print(f'Расчёт заработной платы {float(argv[1]) * float(argv[2]) + int(argv[3])} y.e.')
 
-# def salary():
-#     try:
-#         time = float(input('выработка в часах: '))
-#         pay_rate = float(input('ставка в час: '))
-#         bonus = int(input('премия: '))
-#         return time * pay_rate + bonus
-#     except ValueError:
-#         print('Вводи числа!!!')
-#
-#
-# print(f'Расчёт заработной платы {salary()} у.е.')
+# time, pay_rate, bonus = argv[1:]
+# salary = time * pay_rate + bonus
+# print(f'Расчёт заработной платы {salary} y.e.')
+
+
+# если переменая типа time=8
+# time = float(argv[1][argv[1].find('=')+1:])
+# pay_rate = float(argv[2][argv[2].find('=')+1:])
+# bonus = int(argv[3][argv[3].find('=')+1:])
+# salary = time * pay_rate + bonus
+# print(f'Расчёт заработной платы {salary} y.e.')
+

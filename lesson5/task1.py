c84998f312178de4e7f4bd3e2f9a0e73e329c185
Dubkov <yevgeniydubkov@gmail.com>
2021-01-29 19:31:32 +0300

    done task

--- lesson5/task1.py
+++ lesson5/task1.py
@@ -4,3 +4,19 @@
 Об окончании ввода данных будет свидетельствовать пустая строка.
 '''
 
+# fp = open('task1.txt', 'w')
+# line = input('Введите строку: ')
+# while line:
+#     fp.writelines(f'{line} \n')
+#     line = input('Введите строку: ')
+#     if not line:
+#         break
+# fp.close()
+
+with open('task1.txt', 'w') as fp:
+    line = input('Введите строку: ')
+    while line:
+        fp.writelines(f'{line} \n')
+        line = input('Введите строку: ')
+        if not line:
+            break

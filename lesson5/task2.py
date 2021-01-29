ea27a5c37fadd60fd9d0987dd133f4b020442849
Dubkov <yevgeniydubkov@gmail.com>
2021-01-29 19:31:42 +0300

    try task

--- lesson5/task2.py
+++ lesson5/task2.py
@@ -4,3 +4,19 @@
 выполнить подсчёт строк и слов в каждой строке.
 '''
 
+symbols = []
+with open('task2.txt', 'r') as fp:
+    x = 0
+    y = 0
+    for line in fp:
+        x += 1
+        y = line.count(' ')
+        symbols.append(x)
+        symbols.append(y)
+
+print(x, y, symbols)
+        # for word in line.split():
+        #     y += 1
+        #     print(word)
+        #     print(x)
+        #     print(y)

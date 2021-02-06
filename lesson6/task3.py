65655da420801b7681ae46445380660e0e32d0b6
Dubkov <yevgeniydubkov@gmail.com>
2021-02-06 21:03:29 +0300

    done task

--- lesson6/task3.py
+++ lesson6/task3.py
@@ -11,3 +11,30 @@
 передать данные, проверить значения атрибутов, вызвать методы экземпляров.
 '''
 
+
+class Worker:
+
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        return f'{self.name} {self.surname}'
+
+    def get_total_income(self):
+        return self._income.get('wage') + self._income.get('bonus')
+        # return f'{sum(self._income.values())}'
+
+
+test = Position('Васька', 'Пупкин', 'Слесарь', 50000, 10000)
+print(test.get_full_name())
+print(test.position)
+print(test.get_total_income())
